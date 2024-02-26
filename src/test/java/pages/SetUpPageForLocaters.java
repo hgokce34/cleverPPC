74a7db75db0532240b1e7eba11d57e75eed0a604
Hamza <gokcehamza2934@gmail.com>
2024-02-26 22:47:49 +0300

    Initial commit

---
+++ src/test/java/pages/SetUpPageForLocaters.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utilities.ThreadSafeDriver;
+
+public class SetUpPage extends ParentPage {
+
+
+
+
+}
